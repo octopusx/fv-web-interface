7f3478b633cce814964b5c26001997d7c7b08a3d
Tomasz Fratczak <octopusx1@gmail.com>
2013-03-27 18:55:03 +0000

    added the create and delete slice methods, but have not tested them yet

--- fv_iface.php
+++ fv_iface.php
@@ -44,7 +44,9 @@ class fv_iface{
 	//{"jsonrpc" : "2.0",  "method" : "<method>",  "params" :  <args> ,  "id" : <id>}
 	function getSliceList(){
 		//compile the request in form of an array
-		$request = array("jsonrpc"=>"2.0","method"=>$this->GET_SLICE_LIST,"id"=>$this->request_count);
+		$request = array("jsonrpc"=>"2.0",
+			"method"=>$this->GET_SLICE_LIST,
+			"id"=>$this->request_count);
 		//increase the request id count
 		$this->request_count++;
 		//encode the array in to a json string
@@ -55,22 +57,85 @@ class fv_iface{
 		return json_decode($result,true);
 	}
 
+	// creates a slice with all the appropriate parameters
+	// $name - slice name
+	// $crtl_url - url of the controler to be pointed at
+	// $admin_email - the contact email address of the admin responsible for the slice
+	// $pwd - slice password
+	// $drop_policy - policy for droping packets: "exact" (default) or "rule"
+	// $recv_lldp - boolean, default = false
+	// $flowmod_limit - a number, defaults to -1
+	// $rate_limit - number, defaults to -1
+	// $admin_status - boolean, defaults to true
+	function createSlice($name, $crtl_url, $admin_email, $pwd, $drop_policy, $recv_lldp, $flowmod_limit, $rate_limit, $admin_status){
+		//create the parameter array
+		$params = array("slice-name"=>$name,
+			"controller-url"=>$crtl_url,
+			"admin-contact"=>$admin_email,
+			"password"=>$pwd,
+			"drop-policy"=>$drop_policy,
+			"recv-lldp"=>$recv_lldp,
+			"flowmod-limit"=>$flowmod_limit,
+			"rate-limit"=>$rate_limit,
+			"admin-status"=>$admin_status);
+		//compile the request
+		$request = array("jsonrpc"=>"2.0",
+			"method"=>$this->CREATE_SLICE,
+			"id"=>$this->request_count,
+			"params"=>$params);
+		//increase the request id count
+		$this->request_count++;
+		//encode the array in to a json string
+		$this->requestJson = json_encode($request);
+		//send the json request
+		$result = $this->send($this->requestJson);
+		//decode the result and return it
+		return json_decode($result,true);
+	}
+
+	// removes named slice
+	// $name - slice name
+	function deleteSlice($name){
+		//create the parameter array
+		$params = array("slice-name"=>$name,
+		//compile the request
+		$request = array("jsonrpc"=>"2.0",
+			"method"=>$this->REMOVE_SLICE,
+			"id"=>$this->request_count,
+			"params"=>$params);
+		//increase the request id count
+		$this->request_count++;
+		//encode the array in to a json string
+		$this->requestJson = json_encode($request);
+		//send the json request
+		$result = $this->send($this->requestJson);
+		//decode the result and return it
+		return json_decode($result,true);
+	}
+
+
 	//this function will be used to post the json messages to the api
 	function send($json){
-
+		//initialise the curl connection on the flowvisor api address
 		$ch = curl_init('https://localhost:8080');
+		//set the connection in "POST" mode
 		curl_setopt($ch, CURLOPT_CUSTOMREQUEST, "POST");
+		//set the json string as a post field
 		curl_setopt($ch, CURLOPT_POSTFIELDS, $json);
+		//set the ssl verification
 		curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
 		curl_setopt($ch, CURLOPT_SSL_VERIFYHOST,  2);
+		//set the user and password to access the fv
 		curl_setopt($ch, CURLOPT_USERPWD,$this->user .":" .$this->pwd );
+		//set the curl to return the server output
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+		//initialise the header
 		curl_setopt($ch, CURLOPT_HTTPHEADER, array(
 		    'Content-Type: application/json',
 		    'Content-Length: ' . strlen($json))
 
 		);
-		
+		//execute the connection and receive the response	
 		$result = curl_exec($ch);
 		if (curl_errno($ch)) {
 			print curl_error($ch);
