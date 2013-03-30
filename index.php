ee955dc8c8bab4ace295869d83b664769ba3dd42
Tomasz Fratczak <octopusx@o2.pl>
2013-03-30 00:19:13 +0000

    minor changes to the index file

--- index.php
+++ index.php
@@ -5,34 +5,32 @@
 ?>
 
 <html><body><h1>It works!</h1>
-	<p>This is the default web page for this server.</p>
-	<p>The web server software is running but no content has been added, yet!!</p>
+	<p>This is the index page of the server.</p>
+	<p>It is used to test any functionalities implemented in this project.</p>
 	<?php
 		exec("pgrep gedit", $output, $return);
 		if ($return == 0) {
 		    echo "Ok, process is running<br>\n";
 		}
 
-		//$test = new test;
-		//$test->do_test();
-
-
 		$fv_test = new fv_iface;
 		var_dump($fv_test->createSlice("test","tcp:6633","octopusx@o2.pl","pass","exact",false,-1,-1,true));
-	?><br>
+	?><br><br>
 	<?
 		var_dump($fv_test->getSliceList());
-	?><br>
+	?><br><br>
 	<?
 		var_dump($fv_test->updateSlice("test",null,6634,"octopusx@o2.pl",null,null,null,null,null));
-	?><br>
+	?><br><br>
 	<?
 		var_dump($fv_test->getSliceList());
-	?><br>
+	?><br><br>
 	<?
 		var_dump($fv_test->updateSlicePassword("test","dupa"));
-
-	?><br>
+	?><br><br>
+	<?
+		var_dump($fv_test->getFlowspace("test",null));
+	?><br><br>
 	<?
 		var_dump($fv_test->deleteSlice("test"));
 
