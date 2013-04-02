6973e4566084c7b048337ae917f007697701e59f
Tomasz Fratczak <octopusx@o2.pl>
2013-04-02 15:20:24 +0100

    new index

--- index.php
+++ index.php
@@ -1,38 +1,14 @@
-<?php
-  ini_set('display_errors', 'On');
-//	include("object.php");
-	include("fv_iface.php");	
-?>
+<html>
+	<head></head>
+	<body>
 
-<html><body><h1>It works!</h1>
-	<p>This is the index page of the server.</p>
-	<p>It is used to test any functionalities implemented in this project.</p>
-	<?php
-		exec("pgrep gedit", $output, $return);
-		if ($return == 0) {
-		    echo "Ok, process is running<br>\n";
-		}
+		<h3>Flowvisor Login:</h3>
 
-		$fv_test = new fv_iface;
-		var_dump($fv_test->createSlice("test","tcp:6633","octopusx@o2.pl","pass","exact",false,-1,-1,true));
-	?><br><br>
-	<?
-		var_dump($fv_test->getSliceList());
-	?><br><br>
-	<?
-		var_dump($fv_test->updateSlice("test",null,6634,"octopusx@o2.pl",null,null,null,null,null));
-	?><br><br>
-	<?
-		var_dump($fv_test->getSliceList());
-	?><br><br>
-	<?
-		var_dump($fv_test->updateSlicePassword("test","dupa"));
-	?><br><br>
-	<?
-		var_dump($fv_test->getFlowspace("test",null));
-	?><br><br>
-	<?
-		var_dump($fv_test->deleteSlice("test"));
+		<form action="controller.php?source=login" method="post">
+		User: <input type="text" name="fname">
+		Password: <input type="text" name="age">
+		<input type="submit">
+		</form>
 
-	?>
-</body></html>
+	</body>
+</html>
